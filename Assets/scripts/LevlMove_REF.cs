b13191bf73eb6571ff3d05f1c75347b3540848b3
r1ngoch1 <117596321+r1ngoch1@users.noreply.github.com>
2023-04-12 00:35:09 +0500

    11.04.2023

---
+++ Assets/scripts/LevlMove_REF.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevlMove_REF : MonoBehaviour
+{
+    public int sceneBuildIndex;
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        print("Trigger Entered");
+        
+        if (other.tag == "Player")
+        {
+            print("Switching Scene to " + sceneBuildIndex);
+            SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);
+
+        }
+        
+    }
+}

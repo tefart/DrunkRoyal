67a7cd2536de2967190fbbd2e7f6b17ac305e116
r1ngoch1 <117596321+r1ngoch1@users.noreply.github.com>
2023-04-12 15:17:15 +0500

    12.04.2023/15:16
    Добавил камеру за персонажем

--- Assets/scripts/CameraController.cs
+++ Assets/scripts/CameraController.cs
@@ -1,7 +1,23 @@
 using UnityEngine;
+using System.Collections;
 
 public class CameraController : MonoBehaviour
 {
     [SerializeField] private Transform player;
 
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void LateUpdate()
+    {
+        Vector3 temp = transform.position;
+        temp.x = player.position.x;
+        temp.y = player.position.y;
+
+        transform.position = temp;
+    }
+
 }
+
